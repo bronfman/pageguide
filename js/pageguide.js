e41eea8f0234a5a6b04ce22464d6f20faebf15d2
Spiros Eliopoulos <seliopou@gmail.com>
2012-05-18 09:42:44 -0400

    Revert "Refactor ready code into its own function"
    This reverts commit 3cf322654b039b0dbf83cea55be1ef6a5c8cbf9f.

--- js/pageguide.js
+++ js/pageguide.js
@@ -22,23 +22,37 @@
 tl = window.tl || {};
 tl.pg = tl.pg || {};
 
-tl.pg.prefs = { 
-    'auto_show_first'   : true,
-    'loading_selector'  : '.pgloading',
-    'track_events_cb'   : null
-};
+tl.pg.default_prefs = { 'auto_show_first': true,
+                        'track_events_cb': null };
 
-$(document).ready(function() {
-    var guideWrapper = tl.pg.ensureDivs();
+$(function() {
+    /* page guide object, for pages that have one */
 
-    if (guideWrapper.length) {
-        new tl.pg.PageGuide(guideWrapper);
-        new tl.pg.Listener(guideWrapper, $(tl.pg.prefs.loading_selector)).scheduleCallback();
+    if ($("#tlyPageGuide").length) {
+        var guide = $("#tlyPageGuide");
+
+        var guideWrapper = $('<div/>', {
+            id: 'tlyPageGuideWrapper'
+        }).appendTo('body');
+
+        $('<div/>', {
+            'title': 'Launch Page Guide',
+            'class': 'tlypageguide_toggle',
+            html: 'page guide<div><span>' + guide.data('tourtitle') +
+                    '</span></div><a href="javascript:void(0);" title="close guide">close guide &raquo;</a>'
+        }).appendTo(guideWrapper);
+
+        guide.appendTo(guideWrapper);
+
+        $('<div/>', { id: 'tlyPageGuideMessages' }).appendTo(guideWrapper);
+
+        var pg = new tl.pg.PageGuide($('#tlyPageGuideWrapper'));
+        new tl.pg.Listener(guideWrapper, $('#loading')).scheduleCallback();
     }
 });
 
-tl.pg.PageGuide = function(pg_elem) {
-    this.preferences = $.extend(tl.pg.prefs, {});
+tl.pg.PageGuide = function (pg_elem, preferences) {
+    this.preferences = $.extend(tl.pg.default_prefs, preferences || {});
     this.$base = pg_elem;
     this.$all_items = $('#tlyPageGuide > li', this.$base);
     this.$items = $([]); /* fill me with visible elements on pg expand */
@@ -54,37 +68,6 @@ tl.pg.PageGuide = function(pg_elem) {
     $(document).ready( function() {
         that._on_ready();
     });
-};
-
-tl.pg.ensureDivs = function() {
-    var guideContent = $("#tlyPageGuide"),
-        guideWrapper = $('#tlyPageGuideWrapper');
-
-
-    /* If guideContent doesn't exist, then this page does not support pageguide.
-     * But if guideWrapper exists, then we've already setup pageguide and we
-     * shouldn't do it again.
-     */
-    if (guideContent.length == 0 || guideWrapper.length == 1) {
-        return $();
-    }
-
-    var guideWrapper = $('<div/>', {
-        id: 'tlyPageGuideWrapper'
-    }).appendTo('body');
-
-    $('<div/>', {
-        'title': 'Launch Page Guide',
-        'class': 'tlypageguide_toggle'
-    }).append('page guide')
-      .append('<div><span>' + guideContent.data('tourtitle') + '</span></div>')
-      .append('<a href="javascript:void(0);" title="close guide">close guide &raquo;</a>')
-    .appendTo(guideWrapper);
-
-    guideWrapper.append(guideContent);
-    guideWrapper.append('<div id="tlyPageGuideMessages"/>');
-      
-    return guideWrapper;
 };
 
 tl.pg.isScrolledIntoView = function(elem) {
